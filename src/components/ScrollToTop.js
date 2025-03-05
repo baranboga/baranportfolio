5b1737d443f01ac75f30802432b222dcc6720dae
Rıza Baran Boğa <avbaranboga@gmail.com>
2025-03-05 16:23:18 +0300

    fix

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import ReactGA from "react-ga";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
   useEffect(() => {
     window.scrollTo(0, 0);
+    ReactGA.pageview(pathname); // Track page views
   }, [pathname]);
   return null;
 }
