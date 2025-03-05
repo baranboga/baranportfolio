8c84e4586d20d5d1e7f984c7c7d8c326528bf2e3
Rıza Baran Boğa <avbaranboga@gmail.com>
2025-03-05 16:17:17 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -10,12 +10,16 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
-  Navigate
+  Navigate,
 } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ReactGA from "react-ga";
+
+const TRACKING_ID = "G-D6QL9L7MH4";
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   const [load, upadateLoad] = useState(true);
@@ -39,7 +43,7 @@ function App() {
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
-          <Route path="*" element={<Navigate to="/"/>} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <Footer />
       </div>
